0f182c9e26ee90e2ad0c4eab55b91ba09c12fb62
sahaj jain <37249534+ssaahhaajj@users.noreply.github.com>
2020-03-24 17:26:16 +0530

    Update CPP20Matrix.hpp

--- include/CPP20Matrix.hpp
+++ include/CPP20Matrix.hpp
@@ -1,15 +1,14 @@
-/********************************************************************
-********************************************************************
- Author: Sahaj Jain
- 
- Purpose: Competency Test for the participation in
-          GSOC 2020 in BOOST uBLAS.
-          
-Copyright: It is not allowed to be used for submitting competency
-           test to BOOST GSoC 2020, however, feel free to learn
-           from it.
-********************************************************************
-*********************************************************************/
+/**
+ * 
+ * Author: Sahaj Jain
+ *
+ * Purpose: Competency Test for the participation in
+ *         GSOC 2020 in BOOST uBLAS.
+ *         
+ * Copyright: It is not allowed to be used for submitting competency
+ *          test to BOOST GSoC 2020, however, feel free to learn
+ *          from it.
+ */
 
 #ifndef CPP20MATRIX_H
 #define CPP20MATRIX_H
@@ -21,7 +20,13 @@ Copyright: It is not allowed to be used for submitting competency
 #include <range/v3/view/transform.hpp>
 #include<range/v3/all.hpp>
 
-// Class for matrix using latest features of C++20
+/** @brief CPP20Matrix represents a class for matrix which supports matrix addition 
+ * It uses the latest features of the C++20 
+ * It is assumed that data will be stored in row major policy in the vector
+ * 
+ * @tparam T represent a data type of matrix
+ */
+
 template <class T>
 class CPP20Matrix
 {
@@ -31,18 +36,40 @@ class CPP20Matrix
   
   public:
 
-    // Constructor
+    /** @brief Build an empty Matrix
+     *
+     * @note current Matrix is initialized with zero rows and columns
+     */
     CPP20Matrix() : size_x(0), size_y(0) {}
   
-    // Constructor
+    /** @brief Build a Matrix with no data values
+     *  
+     *  @param n row size
+     *
+     *  @param m column size
+     *
+     * @note current Matrix is constructed with the size specified
+     */
     CPP20Matrix(const std::size_t &n, const std::size_t &m)
         : _array(n * m), size_x(n), size_y(m) {}
 
-    // Constructor
+   /** @brief Build a Matrix with data values
+     *  
+     *  @param n row size
+     *
+     *  @param m column size
+     *
+     *  @param val represents the value with which Matrix is initialized
+     *
+     * @note current Matrix is constructed with the size and value specified
+     */
     CPP20Matrix(const std::size_t &n, const std::size_t &m, const T &val)
         : _array(n * m, val), size_x(n), size_y(m) {}
 
-    // Construction with 2D Vector
+    /** @brief Build a Matrix with 2D vector
+     *
+     *  @param vec represents data for the matrix in 2D vector
+     */
     CPP20Matrix(const std::vector<std::vector<T>> &vec)
         : size_x(vec.size()), size_y((*vec.begin()).size()) {
 
@@ -51,7 +78,10 @@ class CPP20Matrix
         }
     }
 
-    // Construction with 2D initializer_list
+    /** @brief Build a Matrix with 2D initializer list
+     *
+     *  @param l represents data for the matrix in 2D initializer_list
+     */
     CPP20Matrix(const std::initializer_list<std::initializer_list<T>> &l)
         : size_x(l.size()), size_y((*l.begin()).size()) {
         for (auto &it : l) {
@@ -59,20 +89,36 @@ class CPP20Matrix
         }
     }
     
-    // Returns the shape or dimension of Matrix
+    /** @brief determines the shape of matrix
+     */
     constexpr auto shape() const { return std::make_pair(size_x, size_y); }
 
-    // Returns the (i,j)th element of Matrix assuming data is stored in vector in row major form
+    /** @brief determines the (i,j)th element in matrix 
+     *  
+     *  @param i row index
+     *
+     *  @param j column index
+     */
     constexpr inline const T operator()(const std::size_t i, const std::size_t j) const {
         return _array[i * size_y + j];
     }
 
-    // Returns the (i,j)th element of Matrix assuming data is stored in vector in row major form
+     /** @brief determines the (i,j)th element in matrix 
+     *  
+     *  @param i row index
+     *
+     *  @param j column index
+     */
     constexpr inline T &operator()(const std::size_t i, const std::size_t j) {
         return _array[i * size_y + j];
     }
 
-    // Operator Overloading of ostream Operator(<<)
+     /** @brief overloading ostream(<<) operator
+     *  
+     *  @param out ostream object
+     *
+     *  @param other matrix for output
+     */
     friend std::ostream &operator<<(std::ostream &out, CPP20Matrix<T> &other) {
     std::size_t size_x = other.shape().first;
     std::size_t size_y = other.shape().second;
@@ -86,7 +132,10 @@ class CPP20Matrix
     return out;
     }
 
-    // Operator Overloading of Assignment Operator(=)
+    /** @brief overloading Assignment(=) operator
+     *  
+     *  @param other matrix for = operation 
+     */
     constexpr auto& operator=(const CPP20Matrix<T> &other) {
     assert(shape() == other.shape());
     (*this)._array=(other)._array;
@@ -94,7 +143,10 @@ class CPP20Matrix
     return *this;
     }
 
-    // Operator Overloading of Addition Operator(+)
+    /** @brief overloading Addition(+) operator
+     *  
+     *  @param other matrix for addition 
+     */
     constexpr auto operator+(const CPP20Matrix<T> &mat2) {
     assert(shape() == mat2.shape());
     (*this)._array=ranges::views::zip_with(std::plus<>{},(*this)._array,mat2._array);
